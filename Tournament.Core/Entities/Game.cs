5c6e465a1e21d817c39b889feb1e820e21cd1d6e
Clive-Leddy <clive.leddy@gmail.com>
2025-06-26 15:57:51 +0200

    Enhance documentation and relationships in Game and TournamentDetails
    Updated the `Game` class with XML documentation comments and added a navigation property to the `TournamentDetails` entity. Included the necessary `using` directive for foreign key support.
    
    The `TournamentDetails` class was also enhanced with XML documentation to clarify its properties, including the collection of associated games.

--- Tournament.Core/Entities/Game.cs
+++ Tournament.Core/Entities/Game.cs
@@ -1,9 +1,37 @@
-﻿namespace Tournament.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
-public class Game
+namespace Tournament.Core.Entities
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public DateTime Time { get; set; }
-    public int TournamentDetailsId { get; set; }
+    /// <summary>
+    /// Represents a game that is part of a tournament, including its title, scheduled time, 
+    /// and navigation to the associated tournament.
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the game.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title or name of the game.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scheduled time for the game.
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// Gets or sets the foreign key referencing the tournament to which this game belongs.
+        /// </summary>
+        public int TournamentDetailsId { get; set; }
+
+        /// <summary>
+        /// Navigation property to the related <see cref="TournamentDetails"/> entity.
+        /// </summary>
+        [ForeignKey(nameof(TournamentDetailsId))]
+        public TournamentDetails TournamentDetails { get; set; }
+    }
 }
--- Tournament.Core/Entities/TournamentDetails.cs
+++ Tournament.Core/Entities/TournamentDetails.cs
@@ -1,8 +1,29 @@
-﻿namespace Tournament.Core.Entities;
-public class TournamentDetails
+﻿namespace Tournament.Core.Entities
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public DateTime StartDate { get; set; }
-    public ICollection<Game> Games { get; set; } = new List<Game>();
+    /// <summary>
+    /// Represents the details of a tournament, including its title, start date, and associated games.
+    /// </summary>
+    public class TournamentDetails
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the tournament.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title or name of the tournament.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start date of the tournament.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of games associated with the tournament.
+        /// Navigation property representing all games in this tournament.
+        /// </summary>
+        public ICollection<Game> Games { get; set; } = new List<Game>();
+    }
 }
