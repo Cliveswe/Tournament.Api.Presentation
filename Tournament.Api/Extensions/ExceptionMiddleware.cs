08156de1e628720c1a333a6b22a588eafbc7403b
Clive-Leddy <clive.leddy@gmail.com>
2025-07-14 10:34:07 +0200

    Added a custom problem details to display a custom exception. Created a Json file that contains the exception details that in turn is be sent back to the caller.

--- Tournament.Api/Extensions/ExceptionMiddleware.cs
+++ Tournament.Api/Extensions/ExceptionMiddleware.cs
@@ -1,6 +1,8 @@
 ﻿// Ignore Spelling: app Middleware
 
+using Domain.Models.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Tournaments.Api.Extensions;
@@ -15,11 +17,38 @@ public static class ExceptionMiddleware
             {
                 var contextFeatures = context.Features.Get<IExceptionHandlerFeature>();
                 if(contextFeatures is not null) {
-                    var problemDetailsFactory = app.Services.GetServices<ProblemDetailsFactory>();
+                    var problemDetailsFactory = app.Services.GetService<ProblemDetailsFactory>();
                     ArgumentNullException.ThrowIfNull(nameof(problemDetailsFactory));
 
+                    var problemDetails = CreateProblemDetails(context, contextFeatures.Error, problemDetailsFactory, app);
+
+                    context.Response.StatusCode = problemDetails.Status ?? StatusCodes.Status500InternalServerError;//
+
+                    // Serialize the problem details to JSON and write it to the response
+                    await context.Response.WriteAsJsonAsync(problemDetails);
                 }
             });
         });
     }
+
+    private static ProblemDetails CreateProblemDetails(HttpContext context, Exception error, ProblemDetailsFactory? problemDetailsFactory, WebApplication app)
+    {
+
+        return error switch
+        {
+            TournamentNotFoundException tournamentNotFoundException => problemDetailsFactory.CreateProblemDetails(
+                context,
+                StatusCodes.Status404NotFound,
+                title: tournamentNotFoundException.Title,
+                detail: tournamentNotFoundException.Message,
+                instance: context.Request.Path),
+            _ => problemDetailsFactory.CreateProblemDetails(
+                context,
+                StatusCodes.Status500InternalServerError,
+                title: "Internal server error occurred.",
+                detail: app.Environment.IsDevelopment() ? error.Message :
+                    "An unexpected error occurred.")
+        };
+    }
+
 }
