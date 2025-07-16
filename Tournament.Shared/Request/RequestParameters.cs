d46160d6c7f1c7f88f89dcb99239e5605d9a3c94
Clive-Leddy <clive.leddy@gmail.com>
2025-07-16 13:56:55 +0200

    Re-factored PageSize property to limit range of displayed pages .

--- Tournament.Shared/Request/RequestParameters.cs
+++ Tournament.Shared/Request/RequestParameters.cs
@@ -3,11 +3,24 @@
 namespace Tournaments.Shared.Request;
 public class RequestParameters
 {
+    private int pageSize = 20;
+    private const int MaxPageSize = 100;
+    private const int MinPageSize = 2;
+
+    //Set the min and max range for page size.
+    public int PageSize {
+        get => pageSize;
+        set {
+            if(value < MinPageSize || value > MaxPageSize) {
+                pageSize = MaxPageSize; // Default/fall-back page size
+            } else {
+                pageSize = value;
+            }
+        }
+    }
+
     [Range(1, int.MaxValue)]
     public int PageNumber { get; set; } = 1;
-
-    [Range(2, 20)]
-    public int PageSize { get; set; } = 5;
 }
 
 public class TournamentRequestParameters : RequestParameters
