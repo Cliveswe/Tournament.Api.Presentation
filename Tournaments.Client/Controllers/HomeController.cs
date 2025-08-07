4da78c734c6bd5de63debce25a1ffb9799b10f36
Clive-Leddy <clive.leddy@gmail.com>
2025-08-07 10:27:34 +0200

    Added a SimpleGetAsync task to test the client project. Added a base address to the api in the constructor.

--- Tournaments.Client/Controllers/HomeController.cs
+++ Tournaments.Client/Controllers/HomeController.cs
@@ -8,11 +8,18 @@ public class HomeController : Controller
     public HomeController()
     {
         HttpClient = new HttpClient();
+        HttpClient.BaseAddress = new Uri("https://localhost:7225");
     }
 
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var result = await SimpleGetAsync();
         return View();
+    }
+
+    private async Task SimpleGetAsync()
+    {
+        var response = await HttpClient.GetAsync("api/tournaments");
     }
 }
