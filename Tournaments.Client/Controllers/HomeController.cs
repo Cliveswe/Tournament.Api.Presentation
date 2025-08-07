622933ebc719ad6ef41f17fa2f1c1d61377529b4
Clive-Leddy <clive.leddy@gmail.com>
2025-08-07 10:21:45 +0200

    Created a new constructor for the HomeController.

--- Tournaments.Client/Controllers/HomeController.cs
+++ Tournaments.Client/Controllers/HomeController.cs
@@ -3,6 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace Tournaments.Client.Controllers;
 public class HomeController : Controller
 {
+    private HttpClient HttpClient;
+
+    public HomeController()
+    {
+        HttpClient = new HttpClient();
+    }
+
 
     public IActionResult Index()
     {
