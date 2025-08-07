bbd445ddfaa7705a9ac6907d3705e97332a6577a
Clive-Leddy <clive.leddy@gmail.com>
2025-08-07 10:03:41 +0200

    Added a new web app (Model-View-Controller) project for RESTfull http client.

---
+++ Tournaments.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tournaments.Client.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
