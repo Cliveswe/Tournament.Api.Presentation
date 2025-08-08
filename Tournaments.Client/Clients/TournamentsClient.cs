0406ac6b8146c77f37443ed8b0e3dbf317c1c03f
Clive-Leddy <clive.leddy@gmail.com>
2025-08-08 15:45:20 +0200

    Created a TournamnetsClient class that configures the builder.Services.AddHttpClient in the program file.

--- Tournaments.Client/Clients/TournamentsClient.cs
+++ Tournaments.Client/Clients/TournamentsClient.cs
@@ -1,5 +1,20 @@
-﻿namespace Tournaments.Client.Clients;
+﻿// Ignore Spelling: json
+using System.Net.Http.Headers;
+
+namespace Tournaments.Client.Clients;
 
 public class TournamentsClient
 {
+    private const string json = "application/json";
+    private readonly HttpClient client;
+
+    public TournamentsClient(HttpClient client)
+    {
+        this.client = client;
+        this.client.BaseAddress = new Uri("https://localhost:7225");
+        this.client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
+        this.client.Timeout = new TimeSpan(0, 0, 5);//Timeout in 5 seconds.
+    }
+
+
 }
